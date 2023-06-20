cb11fedf37826427e2f6b9d83e86623d6fec4b8b
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-20 21:36:36 +0100

    testing user input

---
+++ src/main/java/task/brilloconnetz/InputValidation/exception/BrilloconnetzException.java
@@ -0,0 +1,7 @@
+package task.brilloconnetz.InputValidation.exception;
+
+public class BrilloconnetzException extends RuntimeException {
+    public BrilloconnetzException(String message) {
+        super(message);
+    }
+}
