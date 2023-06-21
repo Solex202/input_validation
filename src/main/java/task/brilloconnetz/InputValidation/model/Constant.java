5e085853a97b93b22a6b2a514f83a227b856d3d3
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-21 06:52:11 +0100

    testing user input

---
+++ src/main/java/task/brilloconnetz/InputValidation/model/Constant.java
@@ -0,0 +1,8 @@
+package task.brilloconnetz.InputValidation.model;
+
+
+public class Constant {
+    public static final int USERNAME_MIN_LENGTH = 4;
+    public static final int AGE_LIMIT = 16;
+
+}
