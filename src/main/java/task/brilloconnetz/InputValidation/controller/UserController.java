ad210982da298fb6a7a971e2b880a4b751ec6f7d
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-06-21 18:52:19 +0100

    exposing rest api

---
+++ src/main/java/task/brilloconnetz/InputValidation/controller/UserController.java
@@ -0,0 +1,9 @@
+package task.brilloconnetz.InputValidation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class UserController {
+}
